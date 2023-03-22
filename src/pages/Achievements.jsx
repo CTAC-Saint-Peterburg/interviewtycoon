40bb046101229f5d946b84f6a6fa75684395b7d3
Стас <seleznev-00@mail.ru>
2023-03-22 19:14:14 +0300

    add: Achievement page

--- src/pages/Achievements.jsx
+++ src/pages/Achievements.jsx
@@ -1,3 +1,18 @@
+import styles from "./pagesStyleComponents/Achievements.module.css";
 export default function Achievements() {
-  return <div>Achievements page</div>;
+  return (
+    <div className={styles.main}>
+      <h2>Achievements</h2>
+      <div className={styles.achievementsWrapper}>
+        <div className={styles.achievement}>?</div>
+        <div className={styles.achievement}>?</div>
+        <div className={styles.achievement}>?</div>
+        <div className={styles.achievement}>?</div>
+        <div className={styles.achievement}>?</div>
+        <div className={styles.achievement}>?</div>
+        <div className={styles.achievement}>?</div>
+        <div className={styles.achievement}>?</div>
+      </div>
+    </div>
+  );
 }
